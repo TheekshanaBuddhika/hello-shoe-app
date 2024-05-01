d82942c803a606930744494ed6510c8c0a95f576
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-05-01 21:09:43 +0530

    embedded contact  created

--- helloshoebackend/src/main/java/lk/ijse/helloshoebackend/entity/embedded/Contact.java
+++ helloshoebackend/src/main/java/lk/ijse/helloshoebackend/entity/embedded/Contact.java
@@ -1,4 +1,20 @@
 package lk.ijse.helloshoebackend.entity.embedded;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Embeddable
 public class Contact {
+    @Column(length = 20 , unique = true)
+    private String mobile;
+    @Column(length = 20 , unique = true)
+    private String land;
 }
