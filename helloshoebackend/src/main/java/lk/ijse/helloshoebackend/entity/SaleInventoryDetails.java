d93ac9e22a3e287889552309d61a08c7664963ae
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-05-01 21:10:35 +0530

    sale inventory entity  created

--- helloshoebackend/src/main/java/lk/ijse/helloshoebackend/entity/SaleInventoryDetails.java
+++ helloshoebackend/src/main/java/lk/ijse/helloshoebackend/entity/SaleInventoryDetails.java
@@ -1,4 +1,25 @@
 package lk.ijse.helloshoebackend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "sale_inventory_details")
 public class SaleInventoryDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "itm_id", insertable = false, updatable = false)
+    private InventoryEntity inventoryEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id", insertable = false, updatable = false)
+    private SaleEntity saleEntity;
 }
