4b5fd2023d973a734844fc8097bfe9ded7e9892d
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-05-01 20:20:22 +0530

    project created

---
+++ helloshoebackend/src/main/java/lk/ijse/helloshoebackend/HelloshoebackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloshoebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloshoebackendApplication.class, args);
+	}
+
+}
---
+++ helloshoebackend/src/test/java/lk/ijse/helloshoebackend/HelloshoebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloshoebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
