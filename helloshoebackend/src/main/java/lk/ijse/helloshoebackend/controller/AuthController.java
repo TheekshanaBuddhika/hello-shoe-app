2007467e1a441d38110211542b5f804c91b2ceef
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-05-01 21:28:49 +0530

    security jwt layer created

--- helloshoebackend/src/main/java/lk/ijse/helloshoebackend/controller/AuthController.java
+++ helloshoebackend/src/main/java/lk/ijse/helloshoebackend/controller/AuthController.java
@@ -1,7 +1,13 @@
 package lk.ijse.helloshoebackend.controller;
 
 import lk.ijse.helloshoebackend.dto.LoginDTO;
+import lk.ijse.helloshoebackend.security.jwt.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class AuthController {
 
+    @Autowired
+    private  AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtils;
+
     public ResponseEntity<?> systemlogin(@RequestBody LoginDTO loginDTO){
+
+        Authentication authentication= authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        return ResponseEntity.ok(jwt);
 
     }
 
