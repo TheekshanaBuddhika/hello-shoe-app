4edb877acd865afe4d4e76581a8425dd6c04a0b4
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-05-01 21:03:08 +0530

    customer entity created

--- helloshoebackend/src/main/java/lk/ijse/helloshoebackend/entity/embedded/Address.java
+++ helloshoebackend/src/main/java/lk/ijse/helloshoebackend/entity/embedded/Address.java
@@ -1,4 +1,41 @@
 package lk.ijse.helloshoebackend.entity.embedded;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Embeddable
 public class Address {
+    @Column(length = 100)
+    private String line1;
+
+    @Column( length = 100)
+    private String line2;
+
+    @Column(length = 100)
+    private String line3;
+
+    @Column( length = 100)
+    private String line4;
+
+    @Column(length = 100)
+    private String line5;
+
+    @Column(length = 100)
+    private String line6;
+
+    public Address(String lane1, String lane2, String lane3, String lane4, String lane5) {
+        this.line1 = lane1;
+        this.line2 = lane2;
+        this.line3 = lane3;
+        this.line4 = lane4;
+        this.line5 = lane5;
+    }
 }
---
+++ helloshoebackend/src/main/java/lk/ijse/helloshoebackend/util/Constants.java
@@ -0,0 +1,21 @@
+package lk.ijse.helloshoebackend.util;
+
+public enum Constants {
+
+    REGULAR_USER, ADMIN_USER,
+
+    MAN, WOMAN,
+
+    COLOMBO,
+
+    ACTIVE, INACTIVE,
+
+    MARRIED, SINGLE,
+
+    INTERNATIONAL, LOCAL,
+
+    EMPLOYEE_ID, USER_ID, CUSTOMER_ID, SUPPLIER_ID,
+
+    GOLD, SILVER, BRONZE, NEW
+
+}
