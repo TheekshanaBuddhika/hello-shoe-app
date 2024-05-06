10561910ff6940e3a653b0e036ff3594fae742a9
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-05-06 20:42:59 +0530

    structure updated

--- helloshoefrontend/assets/controllers/logincontroller.js
+++ helloshoefrontend/assets/controllers/logincontroller.js
@@ -8,51 +8,48 @@ class user {
   }
 }
 
-$(document).ready(function () {
-  $("#signInBtn").click(function (e) {
-    e.preventDefault();
+$("#signInBtn").click(function (e) {
+  e.preventDefault();
 
-    // Call validateForm function from validation.js
-    if (validateForm()) {
-      var email = $("#userEmail").val();
-      var password = $("#userPassword").val();
-      var data = {
-        email: email,
-        password: password,
-      };
+  // Call validateForm function from validation.js
+  if (validateForm()) {
+    var email = $("#userEmail").val();
+    var password = $("#userPassword").val();
+    var data = {
+      email: email,
+      password: password,
+    };
 
-      console.log(data);
-
-      document.getElementById("dboard-wrapper").style.display = "flex";
-      document.getElementById("login-wrapper").style.display = "none";
-
-      $.ajax({
-        url: BASE_URL + "auth/login",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(data),
-        success: function (res) {
-          console.log(res);
-          localStorage.setItem("user", JSON.stringify(res));
-          (result) => {
-            if (
-              JSON.parse(localStorage.getItem("user")).role === "ADMIN" ||
-              JSON.parse(localStorage.getItem("user")).role === "SUPER_ADMIN"
-            ) {
-              window.location.href = "page/admin/";
-            } else if (
-              JSON.parse(localStorage.getItem("user")).role === "USER"
-            ) {
-            } else {
-              alert("invalid !");
-            }
-          };
-        },
-        error: function (res) {},
-      });
-    } else {
-      // Form validation failed, do nothing or show error message
-      console.log("Form validation failed.");
-    }
-  });
+    console.log(data);
+    document.getElementById("login-wrapper").style.display = "none";
+    document.getElementById("dboard-wrapper").style.display = "flex";
+  } else {
+    // Form validation failed, do nothing or show error message
+    console.log("Form validation failed.");
+  }
 });
+
+// $.ajax({
+//   url: BASE_URL + "auth/login",
+//   type: "POST",
+//   contentType: "application/json",
+//   data: JSON.stringify(data),
+//   success: function (res) {
+//     console.log(res);
+//     localStorage.setItem("user", JSON.stringify(res));
+//     (result) => {
+//       if (
+//         JSON.parse(localStorage.getItem("user")).role === "ADMIN" ||
+//         JSON.parse(localStorage.getItem("user")).role === "SUPER_ADMIN"
+//       ) {
+//         window.location.href = "page/admin/";
+//       } else if (
+//         JSON.parse(localStorage.getItem("user")).role === "USER"
+//       ) {
+//       } else {
+//         alert("invalid !");
+//       }
+//     };
+//   },
+//   error: function (res) {},
+// });
