8bf73876225ae478adbadc9993fda112b505199b
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-05-07 14:48:42 +0530

    js updated

--- helloshoefrontend/assets/controllers/salecontroller.js
+++ helloshoefrontend/assets/controllers/salecontroller.js
@@ -1,4 +1,10 @@
 $("#payment-method-checkbox").click(function () {
-  $("#cash").hide();
-  $("#card").css("display", "flex");
+  var isChecked = $(this).prop("checked");
+  if (isChecked) {
+    $("#card").hide();
+    $("#cash").css("display", "flex");
+  } else {
+    $("#cash").hide();
+    $("#card").css("display", "flex");
+  }
 });
